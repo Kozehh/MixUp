aeabdd23265a6f32087ddb98370616628e4afec7
Anthony <anthony.r@hotmail.fr>
2020-11-16 15:41:24 -0500

    Added song by id

--- MixUp/MixUp/MixUp/Services/SongService.cs
+++ MixUp/MixUp/MixUp/Services/SongService.cs
@@ -1,7 +1,25 @@
-﻿namespace MixUp.Services
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using ClassLibrary.Models;
+using Newtonsoft.Json;
+
+namespace MixUp.Services
 {
     public class SongService
     {
-        
+        private const string trackById = "https://api.spotify.com/v1/tracks/id";
+
+        public async Task<Song> GetSongById(Token token, string id)
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
+
+            var url = trackById.Replace("id", id);
+            var res = await client.GetAsync(url);
+            var json = await res.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Song>(json);
+        }
     }
 }
