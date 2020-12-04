aa49072656bd72ba92167ed1fdd6467125fe5aa0
bigmath <mathieu.pelo@hotmail.ca>
2020-12-03 19:38:29 -0500

    Added 2 tests

---
+++ MixUp/MixUp/MixUp/Tests/GeneralTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MixUp.Tests
+{
+    [TestClass]
+    public class GeneralTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
